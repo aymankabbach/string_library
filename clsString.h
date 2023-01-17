2725c30e5276856e91b9b8e1639f61b85b81efe0
ayman <kabbachayman95@gmail.com>
2023-01-17 21:12:53 +0100

    creating first function in the library

--- clsString.h
+++ clsString.h
@@ -5,4 +5,25 @@ using namespace std;
 class clsString
 {
     
+private:
+    string _value;
+public:
+    clsString()
+    {
+        _value="";
+    }
+    clsString(string value)
+    {
+        _value=value;
+    }
+    void set_value(string value)
+    {
+        _value=value;
+    }
+    string get_value()
+    {
+        return _value;
+    }
+    __declspec(property(get = GetValue, put = SetValue)) string Value; 
+
 };
